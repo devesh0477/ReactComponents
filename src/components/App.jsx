e7617f0fc10b3566dd7c90b3567846f17d0ff7a3
devesh0477 <55842034+devesh0477@users.noreply.github.com>
2020-10-26 12:16:20 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "./heading";
+import List from "./lists";
+
+function appData() {
+  return (
+    <div>
+      <Head />
+      <List />
+    </div>
+  );
+}
+
+export default appData;
